2e81fa92f9bd55a1cdff6db9d0581d4a0fc28cff
ANK Developer <techdeveloper.ank@gmail.com>
2026-09-09 12:05:45 +0530

    set admin password in to env

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,14 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("Admin@123", 10);
+  const adminPassword = process.env.ADMIN_PASSWORD;
+
+  if (!adminPassword) {
+    throw new Error("ADMIN_PASSWORD is missing in .env");
+  }
+
+  const hashedPassword = await bcrypt.hash(adminPassword, 10);
 
   const existing = await prisma.admin.findUnique({
     where: {
-      email: "admin@gmail.com",
+      email: "superadmin@gmail.com",
     },
   });
 
